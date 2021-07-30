6a45afdf50aaec61a0a61aa6574fe9e5f81024bf
Perseus-Lynx <perseusmith73@gmail.com>
2021-07-30 16:07:59 +0200

    Minor improvements

--- PDF scrapper.py
+++ PDF scrapper.py
@@ -1,11 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 import smtplib
+from email.mime.text import MIMEText
 
 webs = []
-
+list_of_pdfs = []
 # You will need to have installed the ChromeDriver to run this program properly
 search_words = input('What do you want to search?').replace(' ', '+')
+
+# To increase the possibilities to find a PDF
+if 'pdf' not in search_words:
+    search_words += ' pdf'
 
 default_url = f"https://www.google.com/search?q={search_words}"
 
@@ -14,7 +19,7 @@ source = requests.get(default_url, cookies=cookies)
 
 soup = BeautifulSoup(source.text, 'html.parser')
 
-list_of_pdfs = []
+
 for link in soup.find_all('a'):
     webs.append(link.get("href"))
 
@@ -39,30 +44,36 @@ url_list()
 
 
 # Sends you the list of the pdf if you want to look them later
+if len(list_of_pdfs) > 0:
+    print('Do you wish that we send you the link through your email?[yes, no]')
+    send_mail = input('> ')
 
-print('Do you wish that we send you the link through your email?[yes, no]')
-send_mail = input('> ')
+    if send_mail.lower() == 'yes':
+        server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
 
-if send_mail.lower() == 'yes':
-    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+        # Replace this values
+        email = "your@email.com"
+        password = "your_password123"
 
-    # Replace this values
-    email = "your@email.com"
-    password = "Yourpassword"
+        pretty_list = ''
 
-    # If you want to send yourself an email you will have to allow less secure apps in google using this link:
-    # (https://www.google.com/settings/security/lesssecureapps)
+        for i in list_of_pdfs:
+            pretty_list += i
+            pretty_list += '\n'
 
-    try:
-        server.login(email, password)
-        if send_mail:
-            server.sendmail(email, email, list_of_pdfs)
-            print("Hopefully you have send yourself an email with the list of pdfs")
+        msg = MIMEText(pretty_list)
 
-    except smtplib.SMTPAuthenticationError:
-        print("Your username or password is incorrect, please write them again")
+        # If you want to send yourself an email you will have to allow less secure apps in google using this link:
+        # (https://www.google.com/settings/security/lesssecureapps)
+        try:
+            server.login(email, password)
+            if send_mail:
+                server.sendmail(email, email, msg.as_string())
+                print("Hopefully you have send yourself an email with the list of pdfs")
 
+        except smtplib.SMTPAuthenticationError:
+            print("Your username or password is incorrect, please write them again")
 
-else:
-    print('These are the pdfs that we found: ')
-    url_list()
+    else:
+        print('These are the pdfs that we found: ')
+        url_list()
